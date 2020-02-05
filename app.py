8ea73e9b12523f82f057301fdb228878c4038e5d
Agzam Idrissov <agzid@win.dtu.dk>
2020-02-05 16:11:03 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def graph():
+    return render_template('graph.html')
+
+#set FLASK_APP=app.py in cmd
+#python -m flask run
+
+@app.route('/about')
+def about():
+    return 'Visualisation for KIT'
+
+if __name__ == "__main__":
+    print('server is running on localhost')
+    app.run(debug=True)
